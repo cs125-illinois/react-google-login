8c3815d60afa086b5f9c56154ca4c00b936df0f2
Geoffrey Challen <challen@illinois.edu>
2019-12-05 21:08:10 -0600

    VS Code configuration.

--- example/src/pages/index.tsx
+++ example/src/pages/index.tsx
@@ -1,4 +1,4 @@
 import { Hello } from "@cs125/google-login"
 import React from "react"
 
-export default () => <Hello compiler="Javascript" framework="Gatsby" />
+export default () => <Hello compiler="TypeScript" framework="Gatsby" />
